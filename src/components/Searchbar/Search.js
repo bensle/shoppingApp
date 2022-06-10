cea02786cb93ac2e09c43aeeeb306863da3fee30
Benjamin Scheffler <scheffler-benjamin@web.de>
2022-06-11 01:03:03 +0200

    Add tolowercase to search

--- src/components/Searchbar/Search.js
+++ src/components/Searchbar/Search.js
@@ -2,25 +2,51 @@ import styled from 'styled-components';
 import { useState } from 'react';
 
 export default function SearchBar(shoppingItems) {
-  const [search, setSearch] = useState('');
-  console.log('inputfiled', search);
+  const shoppingArray = shoppingItems.shoppingItems;
+
+  const [searchInput, setSearchInput] = useState('');
+
+  function onSubmit(event) {
+    event.preventDefault();
+    setSearchInput(event.target.value);
+  }
+
   return (
     <SearchForm>
       <StyledSearchHeading>What do you want to buy?</StyledSearchHeading>
-      <label htmlFor="foodSearch"></label>
+      <label htmlFor="foodSearch">
+        <HiddenSpan>Search food</HiddenSpan>
+      </label>
       <SearchInput
         type="text"
         name="search"
         id="foodSearch"
         placeholder="search..."
-        onChange={(event) => setSearch(event.target.value)}
+        onChange={onSubmit}
       />
+      <StyledList>
+        {shoppingArray
+          .filter((product) =>
+            product.name.en
+              .toLowerCase()
+              .includes(searchInput.toLocaleLowerCase())
+          )
+          .map((product) => (
+            <ListItem key={product._id}>{product.name.en}</ListItem>
+          ))}
+      </StyledList>
 
+      {/* <StyledList>
+        {shoppingArray.map(({ name, _id }) => (
+          <ListItem key={_id}>{name.en}</ListItem>
+        ))}
+      </StyledList> */}
       {/* <button type="submit">Search</button> */}
     </SearchForm>
   );
 }
 
+//------------Styled Comp------------
 const StyledSearchHeading = styled.h2`
   text-align: center;
   font-size: 1rem;
@@ -32,12 +58,35 @@ const SearchForm = styled.form`
   margin-bottom: 15px;
 `;
 
+const HiddenSpan = styled.span`
+  display: none;
+`;
+
 const SearchInput = styled.input`
   height: 30px;
   text-align: center;
   font-family: inherit;
   border-radius: 15px;
   border-style: none;
+  margin-bottom: 5px;
+`;
+
+const StyledList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  margin-left: 10px;
+  margin-right: 10px;
+  background-color: gold;
+`;
+
+const ListItem = styled.li`
+  padding: 10px 15px;
+  border-radius: 12px;
+  background-color: white;
+  flex-grow: 1;
+  text-align: center;
 `;
 
 // const SearchButton = styled.button`
